2068819ea1856757b77d9f5727e6d8879cc94375
Gerald G <geraldguignard2@gmail.com>
2026-05-26 10:49:51 -0400

    - `UserInter` - Made the methods to go into my `UserInterface`

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,4 +1,41 @@
 package com.pluralsight.ui;
 
+import com.pluralsight.order.Order;
+
+import java.util.Scanner;
+
 public class UserInterface {
+
+    private Scanner scanner;
+    private Order currentOrder;
+
+    public UserInterface() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void displayHomeScreen() {
+
+
+    }
+
+    public void newOrder() {
+
+
+    }
+
+    public void addTaco() {
+
+    }
+
+    public void addDrink() {
+
+    }
+
+    public void addChipsAndSalsa() {
+
+    }
+
+    public void checkout() {
+
+    }
 }
