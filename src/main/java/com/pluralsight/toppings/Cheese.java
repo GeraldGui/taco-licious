4182ff83c6610057deea1260ae47a389caab4ca3
Gerald G <geraldguignard2@gmail.com>
2026-05-26 11:50:50 -0400

    - `Meat` and `Cheese` - Extend the parent class into the classes.

--- src/main/java/com/pluralsight/toppings/Cheese.java
+++ src/main/java/com/pluralsight/toppings/Cheese.java
@@ -1,4 +1,10 @@
 package com.pluralsight.toppings;
 
-public class Cheese {
+public class Cheese extends Premium {
+    private boolean hasExtraCheese;
+
+
+    public Cheese(String shell, String size, boolean hasQueso, boolean hasSalsa) {
+        super(shell, size, hasQueso, hasSalsa);
+    }
 }
--- src/main/java/com/pluralsight/toppings/Meat.java
+++ src/main/java/com/pluralsight/toppings/Meat.java
@@ -1,4 +1,10 @@
 package com.pluralsight.toppings;
 
-public class Meat {
+public class Meat extends Premium {
+    private boolean hasExtraMeat;
+
+
+    public Meat(String shell, String size, boolean hasQueso, boolean hasSalsa) {
+        super(shell, size, hasQueso, hasSalsa);
+    }
 }
